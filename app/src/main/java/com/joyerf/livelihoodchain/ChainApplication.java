d26f80031559f6f02c60f3029197c348a87c66fe
joyerf <jiezongchang@foxmail.com>
2018-02-12 15:32:51 +0800

    热更新

--- app/src/main/java/com/joyerf/livelihoodchain/ChainApplication.java
+++ app/src/main/java/com/joyerf/livelihoodchain/ChainApplication.java
@@ -1,22 +1,24 @@
 package com.joyerf.livelihoodchain;
 
-import android.support.multidex.MultiDex;
-import android.support.multidex.MultiDexApplication;
-
-import com.joyerf.livelihoodchain.db.GreenDaoManager;
-import com.orhanobut.logger.Logger;
+import com.tencent.tinker.loader.app.TinkerApplication;
+import com.tencent.tinker.loader.shareutil.ShareConstants;
 
 /**
- * Created by jiezongchang on 2018/1/28.
+ * 自定义Application.
+ *
+ * 注意：这个类集成TinkerApplication类，这里面不做任何操作，所有Application的代码都会放到ApplicationLike继承类当中<br/>
+ * <pre>
+ * 参数解析：
+ * 参数1：int tinkerFlags 表示Tinker支持的类型 dex only、library only or all suuport，default: TINKER_ENABLE_ALL
+ * 参数2：String delegateClassName Application代理类 这里填写你自定义的ApplicationLike
+ * 参数3：String loaderClassName  Tinker的加载器，使用默认即可
+ * 参数4：boolean tinkerLoadVerifyFlag  加载dex或者lib是否验证md5，默认为false
+ * </pre>
  */
+public class ChainApplication extends TinkerApplication {
 
-public class ChainApplication extends MultiDexApplication {
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        MultiDex.install(this);
-        ContextHolder.setContext(this);
-        GreenDaoManager.getInstance();
-        Logger.init("Chain");
+    public ChainApplication() {
+        super(ShareConstants.TINKER_ENABLE_ALL, "com.joyerf.livelihoodchain.ChainApplicationLike",
+                "com.tencent.tinker.loader.TinkerLoader", false);
     }
 }
